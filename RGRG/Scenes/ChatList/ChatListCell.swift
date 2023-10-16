9dc70961a2ab914b16cb1d300ac80526951806c5
jakkujakku <jakkujakku98@gmail.com>
2023-10-16 18:48:25 +0900

    [FEAT]: UITableView 코드 추가

---
+++ RGRG/Scenes/ChatList/ChatListCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatListCell.swift
+//  RGRG
+//
+//  Created by (^ㅗ^)7 iMac on 10/16/23.
+//
+
+import UIKit
+
+class ChatListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
