6100ce6cb2c2e6953f32efa587944e82b7003d63
suzzang_g <hyeonnee98@gmail.com>
2023-10-16 17:29:49 +0900

    [FEAT] : EditProfilePage 레이아웃 구성

--- RGRG/Scenes/EditProfile/EditProfileViewController.swift
+++ RGRG/Scenes/EditProfile/EditProfileViewController.swift
@@ -9,7 +9,20 @@ import SnapKit
 import UIKit
 
 class EditProfileViewController: UIViewController {
-    let testButton = CustomButton(frame: .zero)
+    let profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "photo")
+        return imageView
+    }()
+
+    let userNameTextFieldTitle = CustomLabel()
+    let userNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "닉네임을 입력해주세요"
+        return textField
+    }()
+
+    let noticeLabel = CustomLabel()
 
     deinit {
         print("### NotificationViewController deinitialized")
@@ -20,24 +33,57 @@ extension EditProfileViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.isHidden = false
-        setupButton()
+
+        setNaviGationContorller()
+        configureUI()
     }
 }
 
 extension EditProfileViewController {
-    func setupButton() {
-        view.addSubview(testButton)
-        testButton.configureButton(title: "TEST", cornerValue: 10, backgroundColor: .systemBlue)
-        testButton.addTarget(self, action: #selector(tappedButton), for: .touchUpInside)
-        testButton.snp.makeConstraints { make in
-            make.centerX.centerY.equalToSuperview()
-            make.width.equalTo(150)
-            make.height.equalTo(60)
-        }
+    func setNaviGationContorller() {
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "프로필 수정"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(confirmButtonPressed))
     }
 
-    @objc func tappedButton(_ sender: UIButton) {
-        print("### \(#function)")
+    func setupLabel() {
+        userNameTextFieldTitle.text = "닉네임"
+
+        noticeLabel.text = "닉네임을 입력해주세요!"
+        noticeLabel.setupLabelColor(color: .red)
+    }
+
+    func configureUI() {
+        setupLabel()
+
+        [profileImage, userNameTextFieldTitle, userNameTextField, noticeLabel].forEach { view.addSubview($0) }
+
+        profileImage.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(31)
+            make.width.height.equalTo(165)
+            make.centerX.equalToSuperview()
+        }
+
+        userNameTextFieldTitle.snp.makeConstraints { make in
+            make.top.equalTo(profileImage.snp.bottom).offset(11)
+            make.left.equalToSuperview().offset(35)
+        }
+
+        userNameTextField.snp.makeConstraints { make in
+            make.top.equalTo(userNameTextFieldTitle.snp.bottom).offset(11)
+            make.left.right.equalToSuperview().inset(35)
+            make.height.equalTo(53)
+        }
+
+        noticeLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(35)
+            make.top.equalTo(userNameTextField.snp.bottom).offset(11)
+        }
+    }
+}
+
+extension EditProfileViewController {
+    @objc func confirmButtonPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
     }
 }
