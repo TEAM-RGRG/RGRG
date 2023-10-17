69b22ded1ce1146f3d99b18d911056a67b19dabe
jakkujakku <jakkujakku98@gmail.com>
2023-10-17 16:14:59 +0900

    [FEAT]: ChatListViewController TableView UI 코드 추가

--- RGRG/Scenes/ChatList/ChatListViewController.swift
+++ RGRG/Scenes/ChatList/ChatListViewController.swift
@@ -6,10 +6,11 @@
 //
 
 import SnapKit
+import SwiftUI
 import UIKit
 
 class ChatListViewController: UIViewController {
-    let tableView = CustomTableView(frame: .zero)
+    let tableView = CustomTableView(frame: .zero, style: .insetGrouped)
 
     deinit {
         print("### NotificationViewController deinitialized")
@@ -27,14 +28,65 @@ extension ChatListViewController {
     func setupUI() {
         view.backgroundColor = .systemBackground
         confirmTableView()
+        registerCell()
     }
 
     func confirmTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+
         view.addSubview(tableView)
         tableView.backgroundColor = .systemOrange
         tableView.snp.makeConstraints { make in
-            make.top.leading.trailing.equalToSuperview()
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(10)
         }
     }
+
+    func registerCell() {
+        tableView.register(ChatListCell.self, forCellReuseIdentifier: ChatListCell.identifier)
+    }
 }
+
+// MARK: - TableView Datasource
+
+extension ChatListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 8
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ChatListCell.identifier, for: indexPath) as? ChatListCell else { return UITableViewCell() }
+
+        cell.backgroundColor = .white
+        return cell
+    }
+}
+
+// MARK: - TableView Delegate
+
+extension ChatListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("### \(indexPath.row)")
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+}
+
+//MARK: - SwiftUI Preview
+
+ @available(iOS 13.0, *)
+ struct ChatListViewControllerRepresentble: UIViewRepresentable {
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<ChatListViewControllerRepresentble>) {
+    }
+
+    func makeUIView(context: Context) -> UIView { ChatListViewController().view }
+ }
+
+ @available(iOS 13.0, *)
+ struct ChatListVCPreview: PreviewProvider {
+    static var previews: some View { ChatListViewControllerRepresentble()
+    }
+ }
