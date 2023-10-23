c8d37c7d61ab08465151ba3cbe468363544346ed
jakkujakku <jakkujakku98@gmail.com>
2023-10-23 16:50:46 +0900

    [FEAT]: 파이어베이스 firestore 채팅방 & 채팅 로드 코드 추가

---
+++ RGRG/Models/Channel.swift
@@ -0,0 +1,14 @@
+//
+//  Channel.swift
+//  RGRG
+//
+//  Created by (^ㅗ^)7 iMac on 10/22/23.
+//
+
+import Foundation
+
+struct Channel {
+    var channelName: String
+    var requester: String
+    var writer: String
+}
