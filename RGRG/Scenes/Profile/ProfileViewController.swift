5e94f37128a473030c7786b3c965cbd1e409b031
suzzang_g <hyeonnee98@gmail.com>
2023-10-30 18:11:23 +0900

    [FIX] : 네비게이션 수정

--- RGRG/Scenes/Profile/ProfileViewController.swift
+++ RGRG/Scenes/Profile/ProfileViewController.swift
@@ -77,8 +77,6 @@ extension ProfileViewController {
         configureUI()
         setupProfileView()
         setImageTapGesture()
-        navigationItem.title = "마이페이지"
-        navigationItem.backBarButtonItem?.isHidden = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
