e44798d4332af228192acfb6ec73dab12c673c3d
jakkujakku <jakkujakku98@gmail.com>
2023-11-07 17:39:19 +0900

    [FEAT]: 희망 포지션 입력 안할 시, Alert 경고창 생성 코드 추가

--- RGRG/Scenes/Main/PartyInfoDetailVC.swift
+++ RGRG/Scenes/Main/PartyInfoDetailVC.swift
@@ -256,7 +256,31 @@ class PartyInfoDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+    }
+    
+    // MARK: - ViewWillAppear
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = false
+        setupUI()
+    }
+    
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func menuButtonTapped() {
+        if let user = user {
+            // 자기 자신 user의 uid 필드 필요
+            FireStoreManager.shared.addChannel(channelTitle: party?.writer ?? "n/a", guest: party?.writer ?? "n/a", host: user.uid, channelID: party?.writer ?? "", date: FireStoreManager.shared.dateFormatter(value: Date.now), users: [party?.writer ?? "n/a", user.uid], guestProfile: party?.profileImage ?? "n/a", hostProfile: user.profilePhoto, hostSender: false, guestSender: false) { channel in
+                print("### 채널 추가 하기 :: \(channel)")
+            }
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func setupUI() {
         if party?.writer == user?.uid {
             confirmationButton.isHidden = true
         }
@@ -321,27 +345,6 @@ class PartyInfoDetailVC: UIViewController {
         configureUI()
     }
     
-    // MARK: - ViewWillAppear
-    
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.isHidden = false
-        configureUI()
-    }
-    
-    @objc func backButtonTapped() {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    @objc func menuButtonTapped() {
-        if let user = user {
-            // 자기 자신 user의 uid 필드 필요
-            FireStoreManager.shared.addChannel(channelTitle: party?.writer ?? "n/a", guest: party?.writer ?? "n/a", host: user.uid, channelID: party?.writer ?? "", date: FireStoreManager.shared.dateFormatter(value: Date.now), users: [party?.writer ?? "n/a", user.uid], guestProfile: party?.profileImage ?? "n/a", hostProfile: user.profilePhoto, hostSender: false, guestSender: false) { channel in
-                print("### 채널 추가 하기 :: \(channel)")
-            }
-            navigationController?.popViewController(animated: true)
-        }
-    }
-    
     func configureUI() {
         view.backgroundColor = .rgrgColor5
         
@@ -374,25 +377,25 @@ class PartyInfoDetailVC: UIViewController {
         bottomframeView.addSubview(confirmationButton)
         
         // 네비게이션 바 왼쪽 버튼
-        let backButton = UIButton(type: .custom)
-        backButton.setImage(UIImage(systemName: "chevron.left")?.withRenderingMode(.alwaysTemplate), for: .normal)
-        backButton.tintColor = .black
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        backButton.imageEdgeInsets = .init(top: -18, left: -18, bottom: -18, right: -18)
+//        let backButton = UIButton(type: .custom)
+//        backButton.setImage(UIImage(systemName: "chevron.left")?.withRenderingMode(.alwaysTemplate), for: .normal)
+//        backButton.tintColor = .black
+//        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+//        backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+//        backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+//        backButton.imageEdgeInsets = .init(top: -18, left: -18, bottom: -18, right: -18)
+//
+//        let customItem = UIBarButtonItem(customView: backButton)
+//        navigationItem.leftBarButtonItem = customItem
         
-        let customItem = UIBarButtonItem(customView: backButton)
-        navigationItem.leftBarButtonItem = customItem
-        
-        let menuButton = UIButton(type: .custom)
-        menuButton.setImage(UIImage(named: "verticalEllipsis"), for: .normal)
-        menuButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        menuButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        menuButton.imageEdgeInsets = .init(top: -18, left: -18, bottom: -18, right: -18)
+//        let menuButton = UIButton(type: .custom)
+//        menuButton.setImage(UIImage(named: "verticalEllipsis"), for: .normal)
+//        menuButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+//        menuButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+//        menuButton.imageEdgeInsets = .init(top: -18, left: -18, bottom: -18, right: -18)
         // 네비게이션바 오른쪽 버튼
-        let rightButton = UIBarButtonItem(customView: menuButton)
-        navigationItem.rightBarButtonItem = rightButton
+//        let rightButton = UIBarButtonItem(customView: menuButton)
+//        navigationItem.rightBarButtonItem = rightButton
         
         topFrame.snp.makeConstraints {
             $0.top.leading.equalToSuperview().offset(0)
@@ -547,5 +550,47 @@ class PartyInfoDetailVC: UIViewController {
             $0.height.equalTo(60)
         }
         scrollView.contentSize = contentView.bounds.size
+    }
+}
+
+extension PartyInfoDetailVC {
+    func makeRightBarButton() {
+        // 액션 만들기 >> 메뉴 만들기 >> UIBarButtonItem 만들기
+        let latestSortAction = rightBarButtonItem.makeSingleAction(title: "게시글 수정", attributes: .keepsMenuPresented, state: .off) { _ in
+            print("### 수정하기 알파입니다.")
+            let editVC = CreatePartyVC()
+            
+            self.navigationController?.pushViewController(editVC, animated: true)
+        }
+
+        let bookMarkAction = rightBarButtonItem.makeSingleAction(title: "삭제", attributes: .destructive, state: .off) { [weak self] _ in
+            guard let self = self else { return }
+            
+            PartyManager.shared.deleteParty(thread: partyID) {
+                self.navigationController?.popViewController(animated: true)
+            }
+            print("### 삭제하기 알파입니다.")
+        }
+
+        let menu = [latestSortAction, bookMarkAction]
+
+        let uiMenu = rightBarButtonItem.makeUIMenu(title: "", opetions: .displayInline, uiActions: menu)
+
+        navigationItem.rightBarButtonItem?.changesSelectionAsPrimaryAction = false
+
+        rightBarButtonItem.image = UIImage(named: "verticalEllipsis")
+        rightBarButtonItem.menu = uiMenu
+        rightBarButtonItem.tintColor = UIColor(hex: "#0C356A")
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func makeBackButton() {
+        let backBarButtonItem = UIBarButtonItem(image: UIImage(named: "chevron.left"), style: .plain, target: self, action: #selector(tappedBackButton))
+        backBarButtonItem.tintColor = UIColor(hex: "#0C356A")
+        navigationItem.leftBarButtonItem = backBarButtonItem
+    }
+
+    @objc func tappedBackButton(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
     }
 }
