7105c8921de7023a35e9755716d0a2fdcc3d5167
jakkujakku <jakkujakku98@gmail.com>
2023-11-14 17:49:54 +0900

    [FEAT]: 파티매니저 필터 코드 수정

--- RGRG/Models/PartyManager.swift
+++ RGRG/Models/PartyManager.swift
@@ -12,9 +12,10 @@ import Foundation
 class PartyManager {
     static let shared = PartyManager()
     static let db = Firestore.firestore()
-    func updateParty(tier: [String], position: [String], completion: @escaping ([PartyInfo]) -> Void) {
+    func updateParty(tier: [String], position: [String], iBlocked: [String], youBlocked: [String], completion: @escaping ([PartyInfo]) -> Void) {
         var partyList: [PartyInfo] = []
-
+        var currentUser: User?
+        
         PartyManager.db.collection("party")
             .whereField("tier", in: tier)
             .whereField("position", in: position)
@@ -28,22 +29,25 @@ class PartyManager {
                         for doc in snapshotDocument {
                             let data = doc.data()
                             let thread = doc.documentID
-
+                            
                             if let champions = data["champions"] as? [String], let content = data["content"] as? String, let date = data["date"] as? String, let hopePosition = data["hopePosition"] as? [String], let profileImage = data["profileImage"] as? String, let tier = data["tier"] as? String, let title = data["title"] as? String, let userName = data["userName"] as? String, let writer = data["writer"] as? String, let position = data["position"] as? String {
-                                let party = PartyInfo(champion: champions, content: content, date: date, hopePosition: hopePosition, profileImage: profileImage, tier: tier, title: title, userName: userName, writer: writer, position: position, thread: thread)
-
-                                partyList.append(party)
+                                if let champions = data["champions"] as? [String], let content = data["content"] as? String, let date = data["date"] as? String, let hopePosition = data["hopePosition"] as? [String], let profileImage = data["profileImage"] as? String, let tier = data["tier"] as? String, let title = data["title"] as? String, let userName = data["userName"] as? String, let writer = data["writer"] as? String, let position = data["position"] as? String {
+                                    let party = PartyInfo(champion: champions, content: content, date: date, hopePosition: hopePosition, profileImage: profileImage, tier: tier, title: title, userName: userName, writer: writer, position: position, thread: thread)
+                                    
+                                    if !iBlocked.contains(party.writer), !youBlocked.contains(writer) {
+                                        partyList.append(party)
+                                    }
+                                }
                             }
+                            completion(partyList)
                         }
-                        completion(partyList)
                     }
                 }
             }
     }
-
-    func loadParty(completion: @escaping ([PartyInfo]) -> Void) {
+        
+    func loadParty(iBlocked: [String], youBlocked: [String], completion: @escaping ([PartyInfo]) -> Void) {
         var partyList: [PartyInfo] = []
-        var currentUser: User?
 
         PartyManager.db.collection("party")
             .order(by: "date", descending: true)
@@ -53,28 +57,17 @@ class PartyManager {
                 } else {
                     if let snapshotDocument = querySnapshot?.documents {
                         print("### snapshotDocument \(snapshotDocument)")
-                        FirebaseUserManager.shared.getUserInfo { user in
-                            currentUser = user
-                        }
-
+                        
                         for doc in snapshotDocument {
                             let data = doc.data()
                             let thread = doc.documentID
-
+                                
                             if let champions = data["champions"] as? [String], let content = data["content"] as? String, let date = data["date"] as? String, let hopePosition = data["hopePosition"] as? [String], let profileImage = data["profileImage"] as? String, let tier = data["tier"] as? String, let title = data["title"] as? String, let userName = data["userName"] as? String, let writer = data["writer"] as? String, let position = data["position"] as? String {
                                 let party = PartyInfo(champion: champions, content: content, date: date, hopePosition: hopePosition, profileImage: profileImage, tier: tier, title: title, userName: userName, writer: writer, position: position, thread: thread)
-
-                                if currentUser?.iBlocked.contains(writer) == true {
-                                    print("##### 내가 차단함")
-                                    if currentUser?.youBlocked.contains(currentUser?.uid ?? "n/a") == true {
-                                        print("##### 나도 차단당함")
-                                    }
-                                } else {
-                                    print("##### 잘 모름")
+                                
+                                if !iBlocked.contains(party.writer), !youBlocked.contains(writer) {
                                     partyList.append(party)
                                 }
-
-//                                partyList.append(party)
                             }
                         }
                         completion(partyList)
@@ -82,7 +75,7 @@ class PartyManager {
                 }
             }
     }
-
+        
     func loadMyParty(completion: @escaping ([PartyInfo]) -> Void) {
         var partyList: [PartyInfo] = []
         let current = Auth.auth().currentUser?.uid
@@ -98,10 +91,10 @@ class PartyManager {
                         for doc in snapshotDocument {
                             let data = doc.data()
                             let thread = doc.documentID
-
+                                
                             if let champions = data["champions"] as? [String], let content = data["content"] as? String, let date = data["date"] as? String, let hopePosition = data["hopePosition"] as? [String], let profileImage = data["profileImage"] as? String, let tier = data["tier"] as? String, let title = data["title"] as? String, let userName = data["userName"] as? String, let writer = data["writer"] as? String, let position = data["position"] as? String {
                                 let party = PartyInfo(champion: champions, content: content, date: date, hopePosition: hopePosition, profileImage: profileImage, tier: tier, title: title, userName: userName, writer: writer, position: position, thread: thread)
-
+                                    
                                 partyList.append(party)
                             }
                         }
@@ -110,7 +103,7 @@ class PartyManager {
                 }
             }
     }
-
+        
     func addParty(party: PartyInfo, completion: @escaping (PartyInfo) -> Void) {
         let current = Auth.auth().currentUser?.uid
         PartyManager.db.collection("party")
@@ -136,7 +129,7 @@ class PartyManager {
                 }
             }
     }
-
+        
     func updateParty(party: PartyInfo, thread: String, completion: @escaping (PartyInfo) -> Void) {
         let path = PartyManager.db.collection("party")
         let current = Auth.auth().currentUser?.uid
@@ -154,7 +147,7 @@ class PartyManager {
                          "position": party.position])
         completion(party)
     }
-
+        
     func deleteParty(thread: String, completion: @escaping () -> Void) {
         let path = PartyManager.db.collection("party")
         path
@@ -163,27 +156,27 @@ class PartyManager {
         completion()
     }
 }
-
+    
 extension PartyManager {
     func dateFormatter(strDate: String) -> String {
         let strDateFormatter = DateFormatter()
         strDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         strDateFormatter.timeZone = TimeZone(identifier: "UTC")
         guard var date = strDateFormatter.date(from: strDate) else { return "" }
-
+            
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         dateFormatter.timeZone = TimeZone(identifier: "UTC")
         return dateFormatter.string(from: date)
     }
 }
-
+    
 extension PartyManager {
     func updatePartyStatus(completion: @escaping () -> Void) {
         Firestore.firestore().collection("party")
             .addSnapshotListener { snapshot, _ in
                 guard let snapshot = snapshot else { return }
-
+                    
                 snapshot.documentChanges.forEach { change in
                     switch change.type {
                     case .added:
