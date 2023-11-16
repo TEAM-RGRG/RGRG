2b019b53470944fad645dd4303e11336384450ae
kiakim01 <hnmpot0318@naver.com>
2023-11-16 21:37:47 +0900

    [FIX] ResetPW UI Bug 수정 & 알럿 추가

--- RGRG/Resources/SceneDelegate.swift
+++ RGRG/Resources/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
+        
         self.window = UIWindow(windowScene: windowScene)
-
         var rootViewController = UIViewController()
 
         if Auth.auth().currentUser != nil {
